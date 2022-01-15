c9c2408d26b90e92b942ee9cee01c1de5ca4ec38
syedimran786 <syedimranhgmrd@gmail.com>
2022-01-15 08:34:38 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,33 @@
+// let student={name:'Dinga',age:27,dept:"cse"}
+// console.log(student)
+
+// //! CRUD 
+// //? Create
+// student.marks=420;
+// student['gender']="Male";
+// console.log(student)
+// //? Read
+// console.log(student.name);
+// console.log(student["dept"])
+// console.log(student["age"])
+// //? Update
+// student.age=29;
+// student['marks']=520;
+// console.log(student)
+// //? Delete
+// delete student.dept;
+// delete student["gender"]
+// console.log(student)
+
+let emp={eid:1,ename:"Dinga",esal:20,eage:40,mobile:[9721661294,8800123443,6756453421]};
+
+console.log(emp)
+// console.log(emp.ename)
+// console.log(emp.mobile)
+// console.log(emp.mobile[1])
+
+// for(let key in emp)
+// {
+//     console.log(key,emp[key])
+// }
+
